98b32b8c363dbc09ae7b63720f23099e11e3ac4d
KarlMorten <karl.morten.lunna@gmail.com>
2024-05-23 10:11:00 +0200

    Initial commit

---
+++ Direction.cs
@@ -0,0 +1,43 @@
+﻿namespace Snake
+{
+    public class Direction : IEquatable<Direction>
+    {
+        public readonly static Direction Left = new Direction(0, -1);
+        public readonly static Direction Right = new Direction(0, 1);
+        public readonly static Direction Up = new Direction(-1, 0);
+        public readonly static Direction Down = new Direction(1, 0);
+        public int RowOffset { get; }
+        public int ColOffset { get; }
+
+        private Direction(int rowOffset, int colOffset)
+        {
+            RowOffset = rowOffset;
+            ColOffset = colOffset;
+        }
+
+        public Direction Opposite()
+        {
+            return new Direction(-RowOffset, -ColOffset);
+        }
+
+        public bool Equals(Direction other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return RowOffset == other.RowOffset && ColOffset == other.ColOffset;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Direction)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(RowOffset, ColOffset);
+        }
+    }
+}
---
+++ Position.cs
@@ -0,0 +1,39 @@
+﻿namespace Snake
+{
+    public class Position : IEquatable<Position>
+    {
+        public int Row { get; }
+        public int Col { get; }
+
+        public Position(int row, int col)
+        {
+            Row = row; 
+            Col = col;
+        }
+
+        public Position Translate(Direction dir)
+        {
+            return new Position(Row+dir.RowOffset, Col+dir.ColOffset);
+        }
+
+        public bool Equals(Position other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return Row == other.Row && Col == other.Col;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Position)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Row, Col);
+        }
+    }
+}
